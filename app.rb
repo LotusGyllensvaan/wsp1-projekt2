6f00a15c7b388c08636bd9ab85bc51003c1cdf57
LotusGyllensvaan <lotus.gyllensvaan@elev.ga.ntig.se>
2024-11-18 14:51:04 +0100

    Category och completion

--- app.rb
+++ app.rb
@@ -1,12 +1,80 @@
 class App < Sinatra::Base
 
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/tasks.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
+
     get '/' do
         erb(:"index")
     end
 
     get '/tasks' do
         @tasks = db.execute('SELECT * FROM tasks')
-        erb(:"tasks/index")
+        @ongoing_tasks = db.execute('SELECT * FROM tasks WHERE ongoing = 1')
+        @completed_tasks = db.execute('SELECT * FROM tasks WHERE ongoing = 0')
+        erb(:"index")
+    end
+    post '/tasks' do    
+        db.execute("INSERT INTO tasks (title, description, ongoing, category) VALUES(?,?,?,?)", 
+        [
+            params['task_title'], 
+            params['task_description'],
+            1,
+            params['task_category']
+        ])
+        redirect("/tasks")
     end
 
+
+    post '/tasks/:id/delete' do |id|
+        db.execute('DELETE FROM tasks WHERE Id = ?', id)
+        redirect("/tasks")
+    end
+
+    get '/tasks/:id/edit' do | id |
+        @task = db.execute('SELECT * FROM tasks WHERE Id = ?', id).first
+        erb(:"change")
+    end
+
+    post '/tasks/:id/complete' do | id |
+        db.execute(
+            'UPDATE tasks
+            SET
+                ongoing = 0
+            WHERE Id = ?', id)
+        redirect("/tasks")
+    end
+
+    post '/tasks/:id/uncomplete' do | id |
+        db.execute(
+            'UPDATE tasks
+            SET
+                ongoing = 1
+            WHERE Id = ?', id)
+        redirect("/tasks")
+    end
+
+    post '/tasks/:id/update' do | id |
+        db.execute("
+        UPDATE tasks 
+        SET
+            title = ?,
+            description = ?,
+            category = ?
+        WHERE 
+            id = ?
+        ", 
+        [
+            params['task_title'], 
+            params['task_description'],
+            params['task_category'],
+            id
+        ])
+        redirect("/tasks")
+    end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -16,13 +16,16 @@ class Seeder
     db.execute('CREATE TABLE tasks (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 title TEXT NOT NULL,
-                description TEXT)')
+                description TEXT,
+                ongoing BOOLEAN NOT NULL,
+                category TEXT)')
   end
 
   def self.populate_tables
-    db.execute('INSERT INTO tasks (title, description) VALUES ("Goon sesh")')
-    db.execute('INSERT INTO tasks (title, description) VALUES ("Ägg", "Typ 5 ägg helt ärligt")')
-    db.execute('INSERT INTO tasks (title, description) VALUES ("Träna", "20 sets 30 reps - jamaican spine extensions")')
+    db.execute('INSERT INTO tasks (title, ongoing) VALUES ("Goon sesh", 1)')
+    db.execute('INSERT INTO tasks (title, description, ongoing) VALUES ("Ägg", "Typ 5 ägg helt ärligt", 1)')
+    db.execute('INSERT INTO tasks (title, description, ongoing) VALUES ("Träna", "20 sets 30 reps - jamaican spine extensions", 1)')
+    db.execute('INSERT INTO tasks (title, description, ongoing) VALUES ("Kill", "German Assasinations", 0)')
   end
 
   private
