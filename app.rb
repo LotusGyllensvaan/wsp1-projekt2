a64a8a4764f2f63f1ecba74ba137fb545ca4bc1e
LotusGyllensvaan <lotus.gyllensvaan@elev.ga.ntig.se>
2024-12-09 13:35:07 +0100

    Login

--- app.rb
+++ app.rb
@@ -9,8 +9,33 @@ class App < Sinatra::Base
         return @db
     end
 
+    
+    configure do
+        enable :sessions
+        set :session_secret, SecureRandom.hex(64)
+    end
+
     get '/' do
         redirect("/tasks")
+        if session[:user_id]
+            erb(:"admin/index")
+          else
+            erb :index
+          end
+    end
+
+    get '/admin' do
+        if session[:user_id]
+          erb(:"admin/index")
+        else
+          p "/admin : Access denied."
+          status 401
+          redirect '/unauthorized'
+        end
+    end
+
+    get '/unauthorized' do
+        erb(:unauthorized)
     end
 
     get '/tasks' do
@@ -77,4 +102,41 @@ class App < Sinatra::Base
         ])
         redirect("/tasks")
     end
+
+    post '/login' do
+        request_username = params[:username]
+        request_plain_password = params[:password]
+    
+        user = db.execute("SELECT *
+                FROM users
+                WHERE username = ?",
+                request_username).first
+    
+        unless user
+          p "/login : Invalid username."
+          status 401
+          redirect '/unauthorized'
+        end
+    
+        db_id = user["id"].to_i
+        db_password_hashed = user["password"].to_s
+    
+        # Create a BCrypt object from the hashed password from db
+        bcrypt_db_password = BCrypt::Password.new(db_password_hashed)
+        # Check if the plain password matches the hashed password from db
+        if bcrypt_db_password == request_plain_password
+          session[:user_id] = db_id
+          redirect '/admin'
+        else
+          status 401
+          redirect '/unauthorized'
+        end
+    
+      end
+
+      get '/logout' do
+        p "/logout : Logging out"
+        session.clear
+        redirect '/'
+      end
 end
